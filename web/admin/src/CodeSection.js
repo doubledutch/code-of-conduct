18c0a409d0bd12454efaff6253e5e24a360e89bc
Eddie Stamps <estamps@doubledutch.me>
2018-08-28 16:43:36 +0000

    Formatting

--- web/admin/src/CodeSection.js
+++ web/admin/src/CodeSection.js
@@ -96,55 +96,27 @@ export default class CodeSection extends Component {
 
 }
 
-const defaultCode = `All attendees, speakers, sponsors and volunteers at our event are required to agree with
-the following code of conduct. Organizers will enforce this code throughout the event. We are
-expecting cooperation from all participants to help ensure a safe environment for everybody. 
+const defaultCode = `All attendees, speakers, sponsors and volunteers at our event are required to agree with the following code of conduct. Organizers will enforce this code throughout the event. We are expecting cooperation from all participants to help ensure a safe environment for everybody. 
 
 Unacceptable Behavior 
-The following behaviors are considered harassment and are unacceptable within our 
-community:
+The following behaviors are considered harassment and are unacceptable within our community:
 ● Violence, threats of violence or violent language directed against another person.
-● Sexist, racist, homophobic, transphobic, ableist or otherwise discriminatory jokes
-and language.
+● Sexist, racist, homophobic, transphobic, ableist or otherwise discriminatory jokes and language.
 ● Posting or displaying sexually explicit or violent material.
-● Posting or threatening to post other people’s personally identifying information
-('doxing').
-● Personal insults, particularly those related to gender, sexual orientation, race,
-religion, or disability.
+● Posting or threatening to post other people’s personally identifying information ('doxing').
+● Personal insults, particularly those related to gender, sexual orientation, race, religion, or disability.
 ● Inappropriate photography or recording.
-● Inappropriate physical contact. You should have someone’s consent before
-touching them.
-● Unwelcome sexual attention. This includes, sexualized comments or jokes;
-inappropriate touching, groping, and unwelcomed sexual advances.
+● Inappropriate physical contact. You should have someone’s consent before touching them.
+● Unwelcome sexual attention. This includes, sexualized comments or jokes; inappropriate touching, groping, and unwelcomed sexual advances.
 ● Deliberate intimidation, stalking or following (online or in person).
 ● Advocating for, or encouraging, any of the above behavior.
 ● Sustained disruption of community events, including talks and presentations.
 
 Consequences of Unacceptable Behavior
-Unacceptable behavior from any community member, including sponsors and those with
-decision-making authority, will not be tolerated.
-Anyone asked to stop unacceptable behavior is expected to comply immediately.
-If a community member engages in unacceptable behavior, the community organizers may take
-any action they deem appropriate, up to and including a temporary ban or permanent expulsion
-from the community without warning and without refund.
-We expect participants to follow this code of conduct at all event venues and event related
-social events.
+Unacceptable behavior from any community member, including sponsors and those with decision-making authority, will not be tolerated. Anyone asked to stop unacceptable behavior is expected to comply immediately. If a community member engages in unacceptable behavior, the community organizers may take any action they deem appropriate, up to and including a temporary ban or permanent expulsion from the community without warning and without refund. We expect participants to follow this code of conduct at all event venues and event related social events.
 
 Reporting
-If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as
-possible. Harassment and other code of conduct violations reduce the value of our event for
-everyone. We want you to be safe and happy at our event. People like you make our event a
-better place.
-You can make a report either personally or anonymously.
-Reporting personally can be done directly to event staff and volunteers. They can be
-identified by special badges throughout the event venue.
-Anonymous reporting can be done here.
-Event staff will help participants contact hotel/venue security or local law enforcement,
-provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration
-of the event. We value your attendance.
+If someone makes you or anyone else feel unsafe or unwelcome, please report it as soon as possible. Harassment and other code of conduct violations reduce the value of our event for everyone. We want you to be safe and happy at our event. People like you make our event a better place. You can make a report either personally or anonymously. Reporting personally can be done directly to event staff and volunteers. They can be identified by special badges throughout the event venue. Anonymous reporting can be done here. Event staff will help participants contact hotel/venue security or local law enforcement, provide escorts, or otherwise assist those experiencing harassment to feel safe for the duration of the event. We value your attendance.
 
 Sponsors and exhibitors
-Sponsors and exhibitors are also subject to the anti-harassment policy. In particular, exhibitors
-are asked to not use sexualized images, activities, or other material, and their staff (including
-volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a
-sexualized environment.`
+Sponsors and exhibitors are also subject to the anti-harassment policy. In particular, exhibitors are asked to not use sexualized images, activities, or other material, and their staff (including volunteers) should not use sexualized clothing/uniforms/costumes, or otherwise create a sexualized environment.`
