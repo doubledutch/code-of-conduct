eaf3d9ce2ff5905eae8392007b2e6039aad045d7
Adam Liechty <adam@doubledutch.me>
2019-02-28 16:04:15 -0800

    translations for arabic, german, spanish, french, italian, japanese, dutch

--- web/admin/src/i18n/index.js
+++ web/admin/src/i18n/index.js
@@ -1,11 +1,30 @@
 import enUS from './en-us/code-of-conduct.admin.json'
-// import deDE from './de-de/code-of-conduct.admin.json'
+
+import arINT from './ar-int/code-of-conduct.admin.json'
+import deDE from './de-de/code-of-conduct.admin.json'
+import esES from './es-es/code-of-conduct.admin.json'
+import frFR from './fr-fr/code-of-conduct.admin.json'
+import itIT from './it-it/code-of-conduct.admin.json'
+import jaJP from './ja-jp/code-of-conduct.admin.json'
+import nlNL from './nl-nl/code-of-conduct.admin.json'
 
 export default {
   '': enUS,
   en: enUS,
   'en-US': enUS,
 
-  // "de":    deDE,
-  // "de-DE": deDE,
+  ar: arINT,
+  'ar-INT': arINT,
+  de: deDE,
+  'de-DE': deDE,
+  es: esES,
+  'es-ES': esES,
+  fr: frFR,
+  'fr-FR': frFR,
+  it: itIT,
+  'it-IT': itIT,
+  ja: jaJP,
+  'ja-JP': jaJP,
+  nl: nlNL,
+  'nl-NL': nlNL,
 }
